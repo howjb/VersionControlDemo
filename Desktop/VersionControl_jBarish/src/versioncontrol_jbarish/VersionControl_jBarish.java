2f78c763cd615eedc545d857a7d16e15168722f4
JoshB <howjbarish@gmail.com>
2024-09-30 10:29:26 -0500

    Initial commit

---
+++ Desktop/VersionControl_jBarish/src/versioncontrol_jbarish/VersionControl_jBarish.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package versioncontrol_jbarish;
+
+/**
+ *
+ * @author jawadbarish
+ */
+public class VersionControl_jBarish {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
+
---
+++ Desktop/VersionControl_yourFirstNameLastName/src/main/java/com/m2t2a/versioncontrol_yourfirstnamelastname/VersionControl_yourFirstNameLastName.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.m2t2a.versioncontrol_yourfirstnamelastname;
+
+/**
+ *
+ * @author jawadbarish
+ */
+public class VersionControl_yourFirstNameLastName {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

7e7a9dac54507030349f365cbca3e8deff52a5b9
JoshB <howjbarish@gmail.com>
2024-10-02 18:54:15 -0500

    m6t2

---
+++ VersionControl_JoshBarish/src/versioncontrol_joshbarish/VersionControl_JoshBarish.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package versioncontrol_joshbarish;
+
+/**
+ *
+ * @author jawadbarish
+ */
+public class VersionControl_JoshBarish {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
